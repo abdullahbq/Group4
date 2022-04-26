37dcdb811eeab77407763bfc5502822f8cfaf353
abdullahbq <abdullahbinqueyam@gmail.com>
2022-04-27 01:55:41 +0530

    Update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => {
         </div>
         <div className="col-lg-6">
           <h1 className="fat-text lh-1 mb-3">
-            Responsive left-aligned hero with image
+            Responsive left-aligned Hero with image
           </h1>
           <p className="lead">
             Quickly design and customize responsive mobile-first sites with
